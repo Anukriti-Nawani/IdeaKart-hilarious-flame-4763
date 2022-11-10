de3415a61041ad356452fed69c3d4e5554db9b3d
abhiamber191 <abhiamber191@gmail.com>
2022-11-10 13:27:57 +0530

    done

--- idea_kart/src/App.js
+++ idea_kart/src/App.js
@@ -1,13 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Home from './Dashboard/FunctionalityPart/Home';
-import NewProduct from './Dashboard/FunctionalityPart/NewProduct';
+
 
 function App() {
   return (
     <div className="App">
-    <Home/>
-    <NewProduct/>
+ 
       </div>
   );
 }
