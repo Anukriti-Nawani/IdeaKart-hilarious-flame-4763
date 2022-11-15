5c2807d29b77c16d48a5d19acaefbab63e8c55cc
abhiamber191 <abhiamber191@gmail.com>
2022-11-15 22:58:04 +0530

    title

--- idea_kart/src/App.js
+++ idea_kart/src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import Favicon from "react-favicon";
+
 
 import AllRoutes from './All Routes/Routes';
 
@@ -7,6 +9,9 @@ import AllRoutes from './All Routes/Routes';
 function App() {
   return (
     <div className="App">
+
+    <Favicon url="https://cdn1.vectorstock.com/i/1000x1000/45/05/book-icon-books-in-various-angles-vector-21684505.jpg"></Favicon>
+
       <AllRoutes/>
    
     </div>
